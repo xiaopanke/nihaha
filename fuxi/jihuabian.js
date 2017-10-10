5592ca7075f992e6a05e59fd567c3915425de2ff
xiaopanke <1547947237@qq.com>
2017-10-10 12:30:44 +0800

    复习

---
+++ fuxi/jihuabian.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Administrator on 2017/9/29.
+ */
+
+function () {
+    let obj={};
+
+    //callbacks 创建一个计划
+    obj.callbacks=function(){
+
+    }
+
+    window.$=obj;
+}();
+
+function   callBacks(){
+
+}
+
+function  add(){
+
+}
+
+function remove(){
+
+}
+
+function fire(){
+
+}
