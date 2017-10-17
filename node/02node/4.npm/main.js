1f10a747a81f65193407b3ab9cc0be40325b32b4
xiaopanke <1547947237@qq.com>
2017-10-17 11:09:06 +0800

    component

---
+++ node/02node/4.npm/main.js
@@ -0,0 +1,12 @@
+let str=require('handsome')
+console.log(str);
+
+let str2=require('zfpx')
+console.log(str2);
+
+//exports,require,module,__filename,__dirname
+//console.log(exports);
+console.log(require);
+//console.log(module)
+//console.log(__filename);
+//console.log(__dirname)
