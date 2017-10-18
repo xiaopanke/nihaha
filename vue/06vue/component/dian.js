a399b00bc95bc097915571d506b38f514f62243f
xiaopanke <1547947237@qq.com>
2017-10-18 20:30:31 +0800

    slot router

---
+++ vue/06vue/component/dian.js
@@ -0,0 +1,37 @@
+/*
+function doStuff (x, y, z) {console.log('xixi'); console.log(...args) }
+var args = [0, 1, 2];
+// 调用函数，传递args参数
+//doStuff.apply(null, args);
+doStuff(...args);
+*/
+
+//合并数组
+/*
+let arr1=[1,2,3,4]
+let arr2=[5,6,7,8]
+console.log([...arr1,...arr2])*/
+
+//合并对象
+/*
+let obja={a:1}
+let objb={b:1}
+console.log({...obja,...objb})*/
+
+
+//拷贝数组
+/*
+let arr1=[1,2,3,4]
+let arr2=[...arr1]
+arr2.push(5)
+console.log(arr1,arr2)*/
+
+
+
+
+
+
+
+
+
+
