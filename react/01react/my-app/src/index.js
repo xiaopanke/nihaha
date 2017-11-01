e106c0fab987e7910a2ddfc5d546f125e3e7ecd0
xiaopanke <1547947237@qq.com>
2017-11-01 10:05:25 +0800

    go

--- react/01react/my-app/src/index.js
+++ react/01react/my-app/src/index.js
@@ -1,45 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'
 
-function Avatar(props) {
-  return(
-    <img className="Avatar"
-      src={props.avatarUrl}
-      alt={props.name}
-    />
-  )
+class Clock extends React.Component{
+  render(){
+    return (
+      <div>
+        <h1>panke</h1>
+        <h2>{this.props.date}</h2>
+      </div>
+    )
+  }
 }
 
-function Comment(props) {
-  return (
-    <div className="Comment">
-      <div className="UserInfo">
-        <Avatar {...props.author} />
-        <div className="UserInfo-name">
-          {props.author.name}
-        </div>
-      </div>
-      <div className="Comment-info">
-        <div className="Comment-text">
-          {props.text}
-        </div>
-        <div className="Comment-date">
-          {props.date.toLocaleString()}
-        </div>
-      </div>
-    </div>
-  );
-}
-let props={
-  author:{
-    name:'panke',
-    avatarUrl:'https://www.baidu.com/img/wanshengdoodle_677234cad70a5974a64e4665c6485c71.gif'
-  },
-  text:'今天天气不错',
-  date:new Date()
-}
 ReactDOM.render(
-  <Comment {...props} />,
+  <Clock  date={new Date().toLocaleString()} />,
   document.querySelector('#root')
 )
