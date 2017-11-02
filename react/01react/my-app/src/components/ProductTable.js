133af1326fdda669830928f954d19d8316d39518
xiaopanke <1547947237@qq.com>
2017-11-02 17:48:10 +0800

    大致完成2

--- react/01react/my-app/src/components/ProductTable.js
+++ react/01react/my-app/src/components/ProductTable.js
@@ -5,6 +5,7 @@ export default class ProductTable extends Component{
 
     render(){
         let rows=[];
+        let lastcategory;
         console.log(this.props.products)
         for (var i = 0; i < this.props.products.length; i++) {
           let product=this.props.products[i];
@@ -12,6 +13,10 @@ export default class ProductTable extends Component{
           continue;
           if(this.props.inStockOnly && !product.stocked)
           continue;
+          if(!lastcategory || lastcategory!=product.category){
+            rows.push(<tr><td colSpan={2} style={{fontWeight:'bold'}}>{product.category}</td></tr>)
+            lastcategory=product.category
+          }
           rows.push(<tr key={i}><td style={{color:(product.stocked ? '#000' : 'red')}}>{product.name}</td><td style={{color:(product.stocked ? '#000' : 'red')}}>{product.price}</td></tr>)
         }
         return (
