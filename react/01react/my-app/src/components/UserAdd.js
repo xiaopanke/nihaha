042bb7986053b921462bf95d5248396acd8b8a5a
xiaopanke <1547947237@qq.com>
2017-11-03 16:50:18 +0800

    在跳转的时候进行提示

--- react/01react/my-app/src/components/UserAdd.js
+++ react/01react/my-app/src/components/UserAdd.js
@@ -1,5 +1,12 @@
 import React,{Component} from 'react';
+import {Prompt} from 'react-router-dom'
 export default class UserAdd extends Component{
+  constructor(){
+    super();
+    this.state={
+      blocking:false
+    }
+  }
   handleSubmit=(event) => {
     let name=this.name.value;
     let userstr=localStorage.getItem('users');
@@ -9,20 +16,35 @@ export default class UserAdd extends Component{
       name
     })
     localStorage.setItem('users',JSON.stringify(users))
-    console.log(users)
-    this.props.history.push('/user/list');
+    this.setState({
+        blocking:false
+    },() => {
+        this.props.history.push('/user/list');
+    })
+
+  }
+  handleChange=(event) => {
+    this.setState({
+        blocking:event.target.value && event.target.value.length>0
+    })
   }
   render(){
     return(
-      <form onSubmit={this.handleSubmit}>
-        <div className='form-group'>
-          <label htmlFor="name">姓名</label>
-          <input type="text" className="form-control" ref={ref=>this.name=ref} />
-        </div>
-        <div className='form-group'>
-          <input type="submit" className="btn btn-primary" />
-        </div>
-      </form>
+      <div>
+        <Prompt
+          when={this.state.blocking}
+          message={(location) => (`你确定你要跳转到${location.pathname}吗`)}
+         />
+        <form onSubmit={this.handleSubmit}>
+          <div className='form-group'>
+            <label htmlFor="name">姓名</label>
+            <input type="text" onChange={this.handleChange} className="form-control" ref={ref=>this.name=ref} />
+          </div>
+          <div className='form-group'>
+            <input type="submit" className="btn btn-primary" />
+          </div>
+        </form>
+      </div>
     )
   }
 }
