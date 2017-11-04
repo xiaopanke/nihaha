d3b6764d392bf4b2673ec26cbc18df4bf09c368e
xiaopanke <1547947237@qq.com>
2017-11-04 11:55:13 +0800

    class LikeButton extends Commponent

--- react/02react/reason/button.js
+++ react/02react/reason/button.js
@@ -1,6 +1,7 @@
-class LikeButton{
+class LikeButton extends Commponent{
   constructor(){
     //在构造函数中初始化一个状态对象，这是一个私有变量
+    super();
     this.state={
       isLiked:false
     }
@@ -9,27 +10,7 @@ class LikeButton{
     this.handleClick=this.handleClick.bind(this)
     this.setState=this.setState.bind(this)
   }
-  createDOMFromString (domstr) {
-    let div=document.createElement('div');
-    div.innerHTML=domstr;
-    //返回这个DIV的第一个子元素
-    return div.firstElementChild
-  }
-  setState(newState){
-    //合并旧的状态和新的状态为一个新的状态，再把新的状态赋值给this.state
-    //this.state=Object.assign({},this.stare,newState); //es6
-    this.state={...this.stare,...newState}; //es7
-    console.log(this.state)
-    //先获取这个button的父级节点
-    let parent=this.element.parentNode;
-    //备份一下老节点
-    let oldEle=this.element;
-    this.render(); //render之后会得到一个新的节点
-    //删除老节点
-    parent.removeChild(oldEle)
-    //添加新节点
-    parent.appendChild(this.element)
-  }
+
   handleClick(){
     this.setState({isLiked:!this.state.isLiked})
   }
---
+++ react/02react/reason/component.js
@@ -0,0 +1,24 @@
+class Commponent{
+  createDOMFromString (domstr) {
+    let div=document.createElement('div');
+    div.innerHTML=domstr;
+    //返回这个DIV的第一个子元素
+    return div.firstElementChild
+  }
+  setState(newState){
+    //合并旧的状态和新的状态为一个新的状态，再把新的状态赋值给this.state
+    //this.state=Object.assign({},this.stare,newState); //es6
+    this.state={...this.stare,...newState}; //es7
+    console.log(this.state)
+    //先获取这个button的父级节点
+    let parent=this.element.parentNode;
+    //备份一下老节点
+    let oldEle=this.element;
+    this.render(); //render之后会得到一个新的节点
+    //删除老节点
+    parent.removeChild(oldEle)
+    //添加新节点
+    parent.appendChild(this.element)
+  }
+  
+}
