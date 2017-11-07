464c58aff686d114ce55dd8f907ba339d3f63dd0
xiaopanke <1547947237@qq.com>
2017-11-07 10:32:30 +0800

    exact

--- react/01react/my-app/src/routes/1.basic.js
+++ react/01react/my-app/src/routes/1.basic.js
@@ -8,11 +8,16 @@ function Home(){
 function About(){
   return <div>About</div>
 }
-//Router是路由容器，就是他负责处理在内部定义的所有路由
+/*
+Router是路由容器，就是他负责处理在内部定义的所有路由
+A <Router>一个组件只可能拥有唯一的子元素
+因为默认匹配路径的时候是匹配的前缀，
+
+*/
 ReactDOM.render(
     <Router>
-      <div>
-        <Route path='/' component={Home}/>
+      <div className='container'>
+        <Route exact={true} path='/' component={Home}/>
         <Route path='/about' component={About}/>
       </div>
     </Router>
