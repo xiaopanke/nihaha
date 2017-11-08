97e547fbbf85ce54060d46ee5163f6b9e06c82a5
xiaopanke <1547947237@qq.com>
2017-11-08 12:45:24 +0800

    clone

--- react/01react/my-app/src/index.js
+++ react/01react/my-app/src/index.js
@@ -1,6 +1,6 @@
 
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
-import   './interview/5.lifecycle.js'
+import   './interview/6.clone.js'
 import './transition.css'
 //ReactDOM.render(<Haha />,document.querySelector('#root'))
