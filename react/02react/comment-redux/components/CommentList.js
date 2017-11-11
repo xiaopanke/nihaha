4aaf36287b330e1e98675d09c600b369f45a5fd5
xiaopanke <1547947237@qq.com>
2017-11-11 17:51:52 +0800

    moment

--- react/02react/comment-redux/components/CommentList.js
+++ react/02react/comment-redux/components/CommentList.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import Comment from './Comment';
 export default class CommentList extends React.Component{
+  componentDidMount(){
+    setInterval(() => {
+      console.log('setInterval');
+      this.setState({})
+    },1000)
+  }
   render(){
     return(
       <ul className="list-group">
