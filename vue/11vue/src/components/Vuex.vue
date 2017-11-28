6b94f5e3bdaa6b4e2eacf1e913ef73f32755d5c9
xiaopanke <1547947237@qq.com>
2017-11-28 19:08:34 +0800

    用computed来获取

--- vue/11vue/src/components/Vuex.vue
+++ vue/11vue/src/components/Vuex.vue
@@ -2,7 +2,9 @@
   <div class="">
       {{$store.state.count}}
       {{this.$store.state.count}}
-      
+      <!-- 但是这样获取很麻烦，所以我们可以把这一长串的东西放到computed里面 -->
+      {{count}}
+      {{count2}}
   </div>
 </template>
 
@@ -13,6 +15,14 @@ export default {
     return {
       
     }
+  },
+  computed:{
+      count(){
+        return  this.$store.state.count
+      },
+      count2(){
+        return  this.$store.state.count2
+      }
   }
 }
 </script>
--- vue/11vue/src/store.js
+++ vue/11vue/src/store.js
@@ -3,7 +3,8 @@ import vuex from 'vuex';
 vue.use(vuex);
 //初始化一个数据
 const state={
-    count:0
+    count:0,
+    count2:1
 }
 
 export default new vuex.Store({
