7dfe0a237650dd2ce6f6a6ca2abadf600d218756
李盼珂 <1547947237@qq.com>
2017-12-28 14:38:24 +0800

    Sag

--- parceljs/dist/8fea387c342a81ef9100181e4060fdbd.js
+++ parceljs/dist/8fea387c342a81ef9100181e4060fdbd.js
@@ -79,6 +79,11 @@ console.log('哈哈')
 console.log('哈哈')
 console.log('哈哈')
 document.write('你是谁？？？？？123456')
+let arr=['哈哈','嘿嘿','嘻嘻','嘎嘎'];
+let arr2=arr.filter(item=>(
+  item!='哈哈'
+))
+console.log(arr2)
 
 },{}],0:[function(require,module,exports) {
 var global = (1, eval)('this');
--- parceljs/src/js/index.js
+++ parceljs/src/js/index.js
@@ -11,3 +11,8 @@ console.log('哈哈')
 console.log('哈哈')
 console.log('哈哈')
 document.write('你是谁？？？？？123456')
+let arr=['哈哈','嘿嘿','嘻嘻','嘎嘎'];
+let arr2=arr.filter(item=>(
+  item!='哈哈'
+))
+console.log(arr2)
