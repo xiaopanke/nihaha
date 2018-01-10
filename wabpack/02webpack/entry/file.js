a3ed4fdd9f17d4f8a751cf076dc7b2bbd2ed3956
xiaopanke <1547947237@qq.com>
2018-01-10 10:17:01 +0800

    plugins 插件

---
+++ wabpack/02webpack/entry/file.js
@@ -0,0 +1,8 @@
+function abc() {
+  console.log('abc')
+}
+abc();
+alert(1234)
+console.log('00000000');
+var haha=12313;
+import '../src/js/1.js'
---
+++ wabpack/02webpack/src/js/1.js
@@ -0,0 +1,5 @@
+var xixi='我是1.js文件';
+function abc(name){
+  console.log(name)
+}
+abc('嘻嘻');
